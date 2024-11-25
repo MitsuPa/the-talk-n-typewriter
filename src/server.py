6869dd3c073656c5c6a6e756afa8a79801922966
Mitsuo <80208659+MitsuPa@users.noreply.github.com>
2024-11-25 01:44:07 +0000

    Add Kaggle support

--- src/server.py
+++ src/server.py
@@ -1,4 +1,7 @@
 import asyncio
+import json
+import os
+import tempfile
 from the_talk_n_typewriter import (
     AudioInputRequest,
     TheTalkNTypewriterBase,
@@ -6,12 +9,51 @@ from the_talk_n_typewriter import (
     TranscriptionTaskStatus,
 )
 from grpclib.server import Server
+from dotenv import load_dotenv
+
+if os.getenv("KAGGLE_CONFIG_DIR") is None:
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        os.environ["KAGGLE_CONFIG_DIR"] = tmpdirname
+os.environ["KAGGLE_USERNAME"] = os.getenv("KAGGLE_USERNAME", "example")
+os.environ["KAGGLE_KEY"] = os.getenv("KAGGLE_KEY", "example")
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+api = KaggleApi()
+
+kernel_metadata = {
+    "id": "",
+    "title": "Translation Gummy",
+    "code_file": "code_file.py",
+    "language": "python",
+    "kernel_type": "script",
+    "is_private": True,
+    "enable_gpu": True,
+    "enable_tpu": False,
+    "enable_internet": True,
+    "keywords": [],
+    "dataset_sources": [],
+    "kernel_sources": [],
+    "competition_sources": [],
+    "model_sources": [],
+}
 
 
 class TheTalkNTypewriterService(TheTalkNTypewriterBase):
     async def transcribe(
         self, audio_input_request: AudioInputRequest
     ) -> TranscriptionTask:
+        kaggle_username = os.getenv("KAGGLE_USERNAME")
+        kaggle_key = os.getenv("KAGGLE_KEY")
+        kernel_metadata["id"] = f"{kaggle_username}/translation-gummy"
+        with open(os.path.join(tmpdirname, "kernel-metadata.json"), "w") as new_f:
+            json.dump(kernel_metadata, new_f)
+        push_file = 'print("Pushing code file to Kaggle")'
+        with open(os.path.join(tmpdirname, kernel_metadata["code_file"]), "w") as new_f:
+            new_f.write(push_file)
+        api.authenticate()
+        result = api.kernels_push(tmpdirname)
+        if result is None or result.error:
+            raise Exception(result)
         return TranscriptionTask(id="1", status=TranscriptionTaskStatus.PENDING)
 
 
